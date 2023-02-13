6ea47ac9e5f6f437a54a576f137ecc0a8c0b04dc
d5vis <davisjbanks@gmail.com>
2023-02-13 02:32:31 -0800

    add track changing

--- src/Button.js
+++ src/Button.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Button() {
   // TODO:
   // add hourly music
   // add volume controller
   // add themes/game select
-  let url =
+
+  // also change these variable names and store all
+  let url6am =
     "https://vgmsite.com/soundtracks/animal-crossing-wild-world/cnwnmvft/39%206%20AM%20%28AC_WW%29.mp3";
-  const [audio, setAudio] = useState(new Audio(url));
+  let url7am =
+    "https://vgmsite.com/soundtracks/animal-crossing-wild-world/xqxtybnu/40%207%20AM%20%28AC_WW%29.mp3";
+  const [audio, setAudio] = useState(new Audio(url6am));
   const [playing, setPlaying] = useState(false);
+  audio.volume = 0.5;
   audio.loop = true;
 
   function playAudio() {
-    console.log(playing);
     if (!playing) {
       audio.play();
     } else {
@@ -21,5 +25,16 @@ export default function Button() {
     setPlaying(!playing);
   }
 
-  return <button onClick={playAudio}>start/stop session</button>;
+  function changeTrack() {
+    // TODO: fade in fade out
+    audio.src = url7am;
+    audio.play();
+  }
+
+  return (
+    <div>
+      <button onClick={playAudio}>start/stop session</button>
+      <button onClick={changeTrack}>(testing) change track</button>
+    </div>
+  );
 }
