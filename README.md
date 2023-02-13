85a2ffe2187361e333a9f918fdb3eb5b2a638a7c
d5vis <davisjbanks@gmail.com>
2023-02-13 12:21:00 -0800

    wild world json, hourly audio update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # animal crossing hourly music
 
-24 hour clock that plays hourly music from animal crossing
+24 hour clock that plays hourly music from animal crossing.
+
+currently supports wild world, other games will be supported soon.
+
+[animal crossing hourly music](https://d5vis.github.io/ac/)
--- src/Button.js
+++ src/Button.js
@@ -1,20 +1,41 @@
 import { useState, useEffect } from "react";
+import Ac from "./ac.json";
 
 export default function Button() {
   // TODO:
-  // add hourly music
   // add volume controller
   // add themes/game select
-
-  // also change these variable names and store all
-  let url6am =
-    "https://vgmsite.com/soundtracks/animal-crossing-wild-world/cnwnmvft/39%206%20AM%20%28AC_WW%29.mp3";
-  let url7am =
-    "https://vgmsite.com/soundtracks/animal-crossing-wild-world/xqxtybnu/40%207%20AM%20%28AC_WW%29.mp3";
-  const [audio, setAudio] = useState(new Audio(url6am));
   const [playing, setPlaying] = useState(false);
+  const [hours, setHours] = useState(new Date().getHours());
+  const [track, setTrack] = useState(hours + 1);
+  const [game, setGame] = useState("wildWorld");
+  const [audio, setAudio] = useState(new Audio());
   audio.volume = 0.5;
   audio.loop = true;
+
+  // runs every 10 seconds to check hour
+  useEffect(() => {
+    setInterval(() => {
+      //console.log("checking hour: " + hours);
+      setHours(new Date().getHours());
+    }, 10000);
+  }, []);
+
+  // update track on hour change
+  useEffect(() => {
+    // TODO: update for game select
+    setTrack(Ac.games.wildWorld[hours - 1]);
+  }, [hours]);
+
+  useEffect(() => {
+    // TODO: fade in fade out
+
+    audio.src = track;
+
+    if (playing) {
+      audio.play();
+    }
+  }, [track]);
 
   function playAudio() {
     if (!playing) {
@@ -25,16 +46,18 @@ export default function Button() {
     setPlaying(!playing);
   }
 
+  // for testing
   function changeTrack() {
-    // TODO: fade in fade out
-    audio.src = url7am;
+    setTrack(Ac.games.wildWorld[1]);
+    audio.src = track;
+    console.log(audio.src);
     audio.play();
   }
 
-  return (
-    <div>
-      <button onClick={playAudio}>start/stop session</button>
-      <button onClick={changeTrack}>(testing) change track</button>
-    </div>
-  );
+  function changeHour() {
+    setHours(2);
+    console.log(hours);
+  }
+
+  return <button onClick={playAudio}>start/stop session</button>;
 }
